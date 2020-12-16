2d31a44158188be13e3ddb32ff99770d9473a415
hbiede <6586509+hbiede@users.noreply.github.com>
2020-12-16 01:44:56 -0600

    Day 16 tests

--- src/day16/__tests__/dayTest.ts
+++ src/day16/__tests__/dayTest.ts
@@ -1,16 +1,29 @@
-import { part1, part2 } from '../solution';
+import { part1 } from '../solution';
 import { inputAsStringArray } from '../../utils/InputProcessor';
 
 const input = inputAsStringArray('src/day16/input.txt');
 
-const inputExample = [''];
+const inputExample = [
+  'class: 1-3 or 5-7',
+  'row: 6-11 or 33-44',
+  'seat: 13-40 or 45-50',
+  '',
+  'your ticket:',
+  '7,1,14',
+  '',
+  'nearby tickets:',
+  '7,3,47',
+  '40,4,50',
+  '55,2,20',
+  '38,6,12',
+];
 
 describe('day 16', () => {
   it('should solve part 1 with example input', () => {
-    expect(part1(inputExample)).toBe(127);
+    expect(part1(inputExample)).toBe(71);
   });
   it('should solve part 1 with my assigned input', () => {
-    expect(part1(input)).toBe(393911906);
+    expect(part1(input)).toBe(24021);
   });
   // eslint-disable-next-line jest/no-commented-out-tests
   // it('should solve part 2 with example input', () => {
