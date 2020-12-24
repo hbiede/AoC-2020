721186910bb50c6a679d9f8589ab64ec5491f1e8
hbiede <6586509+hbiede@users.noreply.github.com>
2020-12-24 13:47:55 -0600

    Day 24 tests

--- src/day24/__tests__/dayTest.ts
+++ src/day24/__tests__/dayTest.ts
@@ -3,19 +3,40 @@ import { inputAsStringArray } from '../../utils/InputProcessor';
 
 const input = inputAsStringArray('src/day24/input.txt');
 
-const inputExample = [''];
+const inputExample = [
+  'sesenwnenenewseeswwswswwnenewsewsw',
+  'neeenesenwnwwswnenewnwwsewnenwseswesw',
+  'seswneswswsenwwnwse',
+  'nwnwneseeswswnenewneswwnewseswneseene',
+  'swweswneswnenwsewnwneneseenw',
+  'eesenwseswswnenwswnwnwsewwnwsene',
+  'sewnenenenesenwsewnenwwwse',
+  'wenwwweseeeweswwwnwwe',
+  'wsweesenenewnwwnwsenewsenwwsesesenwne',
+  'neeswseenwwswnwswswnw',
+  'nenwswwsewswnenenewsenwsenwnesesenew',
+  'enewnwewneswsewnwswenweswnenwsenwsw',
+  'sweneswneswneneenwnewenewwneswswnese',
+  'swwesenesewenwneswnwwneseswwne',
+  'enesenwswwswneneswsenwnewswseenwsese',
+  'wnwnesenesenenwwnenwsewesewsesesew',
+  'nenewswnwewswnenesenwnesewesw',
+  'eneswnwswnwsenenwnwnwwseeswneewsenese',
+  'neswnwewnwnwseenwseesewsenwsweewe',
+  'wseweeenwnesenwwwswnew',
+];
 
 describe('day 24', () => {
   it('should solve part 1 with example input', () => {
-    expect(part1(inputExample)).toBe(127);
+    expect(part1(inputExample)).toBe(10);
   });
   it('should solve part 1 with my assigned input', () => {
-    expect(part1(input)).toBe(393911906);
+    expect(part1(input)).toBe(339);
   });
   it('should solve part 2 with example input', () => {
-    expect(part2(inputExample)).toBe(62);
+    expect(part2(inputExample)).toBe(2208);
   });
   it('should solve part 2 with my assigned input', () => {
-    expect(part2(input)).toBe(59341885);
+    expect(part2(input)).toBe(3794);
   });
 });
